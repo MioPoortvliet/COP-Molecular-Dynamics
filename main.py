4c89da0220fe77ef7c11e263bbc000cb2f89f1cb
Jonah Post <48297677+JonahPost@users.noreply.github.com>
2021-02-08 23:32:01 +0100

    updated the animation class
    now includes text of frame index
    
    sorry also had to change indentation in the main, such that spyder liked it.

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from src.animation import Animation
 import numpy as np
 
 def main() -> None:
-	dimensions = 2
-	particles = 20
-	box_size = 5
-
-	timestep = 1e-9
-
-	sim = Simulation(particles, dimensions, box_size=box_size, time_step=timestep, end_time=timestep*5e3, vel_max=1)
-	sim.run_sim()
-	plot_positions(sim.positions, sim.velocities, sim.end_time)
-	print(np.mean(sim.velocities[-1,::,::]), np.std(sim.velocities[-1,::,::]), 1e-4/1e-5)
-	ani = Animation(sim.positions, box_size=box_size, dimension=dimensions, frameskip=50)
-	ani.run()
+    dimensions = 2
+    particles = 20
+    box_size = 5
+    
+    timestep = 1e-9
+    
+    sim = Simulation(particles, dimensions, box_size=box_size, time_step=timestep, end_time=timestep*5e3, vel_max=1)
+    sim.run_sim()
+    plot_positions(sim.positions, sim.velocities, sim.end_time)
+    print(np.mean(sim.velocities[-1,::,::]), np.std(sim.velocities[-1,::,::]), 1e-4/1e-5)
+    ani = Animation(sim.positions, box_size=box_size, dimension=dimensions, frameskip=50)
+    ani.run()
 
 
 if __name__ == "__main__":
--- src/animation.py
+++ src/animation.py
@@ -35,22 +35,20 @@ class Animation:
             self.ax = self.fig.add_subplot(111)
             self.ax.set_aspect('equal')
             self.scat = self.ax.scatter(self.positions[0,::,0], self.positions[0,::,1])
-            self.ax.set_xlim([0, self.box_size[0]])
-            self.ax.set_ylim([0, self.box_size[1]])
-            self.ax.set_xlabel('X')
-            self.ax.set_ylabel('Y')
-
+            self.time_text = self.ax.text(.02, .02, '', transform=self.ax.transAxes, color="black")
         elif self.dimension == 3:
-            self.ax = p3.Axes3D(self.fig)
-
+            self.ax = self.fig.gca(projection='3d')
+            self.ax.auto_scale_xyz([0, self.box_size[0]],[0, self.box_size[1]],[0, 5*self.box_size[2]])
             self.scat = self.ax.scatter(self.positions[0,::,0],self.positions[0,::,1],self.positions[0,::,2])
-            self.ax.set_xlim3d([0, self.box_size[0]])
-            self.ax.set_ylim3d([0, self.box_size[1]])
+            self.time_text = self.ax.text2D(.02, .02, '', transform=self.ax.transAxes, color="black")
             self.ax.set_zlim3d([0, self.box_size[2]])
-            self.ax.set_xlabel('X')
-            self.ax.set_ylabel('Y')
             self.ax.set_zlabel('Z')
-
+        
+        self.ax.set_xlim([0, self.box_size[0]])
+        self.ax.set_ylim([0, self.box_size[1]])
+        self.ax.set_xlabel('X')
+        self.ax.set_ylabel('Y')
+        self.ax.set_title('Molecular dynamics')
 
     def run(self):
         if self.dimension == 2:
@@ -64,9 +62,16 @@ class Animation:
     def update2d(self, i):
         self.time_index += self.frameskip
         self.scat._offsets = self.positions[self.time_index,::,::]
+        self.time_text.set_text('frame = %.1f' % self.time_index)
 
 
     def update3d(self, i):
         self.time_index += self.frameskip
         self.scat._offsets3d = (self.positions[self.time_index,::,0], self.positions[self.time_index,::,1], self.positions[self.time_index,::,2])
-        
+        self.time_text.set_text('frame = %.1f' % self.time_index)
+""""
+    maybe we should consider running the aimation during the simulation.
+    Then we do not need to save all data, and we can save time/space for larger simulations
+    Now it takes enourmous times to do a simulation for 5e5 steps for example
+    Or we could just save the data with frameskip intervals.
+"""
